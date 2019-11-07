d0b57c01d76a87d9f80675ec957e602a8516d6b2
Bashir Momen <bm@leanlinking.com>
2019-11-07 15:42:20 +0100

    Cleanup main function

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -18,27 +18,29 @@ namespace ConsoleApp
 
         static void Main()
         {
-            Console.WriteLine("1. how many ints are there? {0}", _listService.Count(ints));
+            Console.WriteLine("The list Contains: {0}", string.Join(" ", ints));
 
-            Console.WriteLine("2. sum of all ints? {0}", _listService.Sum(ints));
+            Console.WriteLine("\n1.How many ints are there? {0}", _listService.Count(ints));
 
-            Console.WriteLine("3. average of all ints? {0}", _listService.Average(ints));
+            Console.WriteLine("\n2.Sum of all ints? {0}", _listService.Sum(ints));
 
-            Console.WriteLine("4. what is the number at position 2? {0}", _listService.ValueAt(ints, 2));
+            Console.WriteLine("\n3.Average of all ints? {0}", _listService.Average(ints));
 
-            Console.WriteLine("5. What is the position for 4? {0}", _listService.GetPosition(ints, 4));
+            Console.WriteLine("\n4.What is the number at position 2? {0}", _listService.ValueAt(ints, 2));
 
-            Console.WriteLine("6. exclude all ints larger than 3? {0}", _listService.RemoveLargerThan(ints, 3));
+            Console.WriteLine("\n5.What is the position for 4? {0}", _listService.GetPosition(ints, 4));
 
-            Console.WriteLine("7. make one big int out of all the ints? {0}", _listService.BigInt(ints));
+            Console.WriteLine("\n6.Exclude all ints larger than 3? {0}", string.Join(" ", _listService.RemoveLargerThan(ints, 3)));
 
-            Console.WriteLine("8. ints where next int is even? {0}", _listService.NextIsEven(ints));
+            Console.WriteLine("\n7.Make one big int out of all the ints? {0}", _listService.BigInt(ints));
 
-            Console.WriteLine("9. replace each int with sum of previous and next value? {0}", _listService.NextIsEven(ints));
+            Console.WriteLine("\n8.Ints where next int is even? {0}", string.Join(" ", _listService.NextIsEven(ints)));
 
-            Console.WriteLine("10. Reverse order of elements? {0}", _listService.Reverse(ints));
+            Console.WriteLine("\n9.Replace each int with sum of previous and next value? {0}", string.Join(" ", _listService.NextIsEven(ints)));
 
-            Console.WriteLine("11. what is the diff between ints and subSet? {0}", _listService.Difference(ints, new List<int> { 1, 5 }));
+            Console.WriteLine("\n10.Reverse order of elements? {0}", string.Join(" ", _listService.Reverse(ints)));
+
+            Console.WriteLine("\n11.What is the diff between ints and subSet? {0}", string.Join(" ", _listService.Difference(ints, new List<int> { 1, 5 })));
 
             Console.ReadKey();
         }
